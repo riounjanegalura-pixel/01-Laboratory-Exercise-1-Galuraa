2f3b30cf2489e8306e6a22a27946dbf2c2828d6f
riounjanegalura-pixel <riounjanegalura@gmail.com>
2025-08-15 22:20:01 +0800

    static return type method

--- WindowsFormsApp1/StudentInfo.cs
+++ WindowsFormsApp1/StudentInfo.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WindowsFormsApp1
 {
@@ -25,6 +27,53 @@ namespace WindowsFormsApp1
             public static long StudentNo = 0;
         }
 
+        public static string GetFirstName(string FirstName)
+        {
+            StudentInfoClass.FirstName = FirstName;
+            return FirstName;
+        }
 
+        public static string GetLastName(string LastName)
+        {
+            StudentInfoClass.LastName = LastName;
+            return LastName;
+        }
+
+        public static string GetMiddleName(string MiddleName)
+        {
+            StudentInfoClass.MiddleName = MiddleName;
+            return MiddleName;
+        }
+
+        public static string GetAddress(string Address)
+        {
+            StudentInfoClass.Address = Address;
+            return Address;
+        }
+
+        public static string GetProgram(string Program)
+        {
+            StudentInfoClass.Program = Program;
+            return Program;
+        }
+
+        public static long GetAge(long Age)
+        {
+            StudentInfoClass.Age = Age;
+            return Age;
+        }
+
+        public static long GetContactNo(long ContactNo)
+        {
+            StudentInfoClass.ContactNo = ContactNo;
+            return ContactNo;
+        }
+
+        public static long GetStudentNo(long StudentNo)
+        {
+            StudentInfoClass.StudentNo = StudentNo;
+            return StudentNo;
+
+        }
     }
 }
