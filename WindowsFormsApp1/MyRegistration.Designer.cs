786d1285b11aef58c3932079a9e0ab262fffab94
riounjanegalura-pixel <riounjanegalura@gmail.com>
2025-08-15 22:13:56 +0800

    (2) delegates based

--- WindowsFormsApp1/MyRegistration.Designer.cs
+++ WindowsFormsApp1/MyRegistration.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp1
 {
-    partial class Form1
+    partial class MyRegistration
     {
         /// <summary>
         /// Required designer variable.
@@ -191,8 +191,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.DarkKhaki;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(745, 431);
             this.Controls.Add(this.btnNext);
             this.Controls.Add(this.rtxtAddress);
             this.Controls.Add(this.cbProgram);
---
+++ WindowsFormsApp1/StudentInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class StudentInfo
+    {
+        public class StudentInfoClass
+        {
+            // Delegates
+            public delegate void DelegateNumber(long number);
+            public delegate void DelegateText(string txt);
+
+        }
+
+
+    }
+}
